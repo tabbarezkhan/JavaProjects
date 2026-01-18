6aa7efc8888809100cd3dc393e7d4fd0be7d2a2e
Tabbarez Khan <tabbarezkhan@gmail.com>
2026-01-18 23:23:22 +1100

    Adding a new file to check FlatMap

---
+++ PureJavaCollectionInterviewPrep/src/main/java/org/example/FlatMapExample.java
@@ -0,0 +1,19 @@
+package org.example;
+
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class FlatMapExample {
+    public static void main(String args[])
+    {
+        List<List<Integer>>list =Arrays.asList(
+                Arrays.asList(1,2),
+                Arrays.asList(3,4),
+                Arrays.asList(5,6)
+        );
+
+        System.out.println(list.stream().flatMap(List::stream).collect(Collectors.toList()));
+
+    }
+}
