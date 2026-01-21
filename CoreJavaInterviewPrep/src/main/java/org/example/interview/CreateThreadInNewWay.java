9dcd0c7873fdf4458592a3a47d794c393dd3742a
Tabbarez Khan <tabbarezkhan@gmail.com>
2026-01-21 16:16:33 +1100

    Update CreateThreadInNewWay.java

--- CoreJavaInterviewPrep/src/main/java/org/example/interview/CreateThreadInNewWay.java
+++ CoreJavaInterviewPrep/src/main/java/org/example/interview/CreateThreadInNewWay.java
@@ -7,8 +7,19 @@ import java.util.concurrent.Executors;
 public class CreateThreadInNewWay {
     public static void main(String[] args) {
         ExecutorService threadService = Executors.newFixedThreadPool(3);
-        Runnable task1 = () -> System.out.println("Thread1");
-        Runnable task2 = () -> System.out.println("Thread2");
+        Runnable task1 = () -> {
+            System.out.println("Thread1");
+            try { Thread.sleep(1000); } catch (InterruptedException e) {}
+            System.out.println("Thread 1 finished");
+        };
+        Runnable task2 = () -> {
+
+            System.out.println("Thread2");
+            try { Thread.sleep(1000); } catch (InterruptedException e) {}
+            System.out.println("Thread 2 finished");
+        };
+        task1.run();
+        task2.run();
         task1.run();
         task2.run();
     }
